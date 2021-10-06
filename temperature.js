c4ad28120a5b6fc61b2dac5d5235e2282427267e
AMMAR AL HADDAD <91353209+ammaralhaddad@users.noreply.github.com>
2021-10-06 17:10:05 -0400

    Create temperature.js
    "Use conditionals to tell people about the weather"

---
+++ temperature.js
@@ -0,0 +1,11 @@
+const temperature = 12;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a coat!");
+} else if (temperature < 15) {
+  console.log("you need a sweater");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
