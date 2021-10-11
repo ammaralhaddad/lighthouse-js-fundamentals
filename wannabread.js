efe7e9b106cc7297aa2dfbd7c4bc2c1bd90f5407
AMMAR AL HADDAD <91353209+ammaralhaddad@users.noreply.github.com>
2021-10-11 12:32:34 -0400

    Create wannabread.js
    For, while loop

---
+++ wannabread.js
@@ -0,0 +1,27 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+let i =0 
+
+while(i<ingredients.length){
+console.log(packingList[i]);
+i++
+
+}
+
+for (let i=0;i>ingredients.length;i++){
+
+
+consoler.log(ingredients[i])
+
+}
+
+
+for(let i=7;i<ingredients.length;i--){
+
+  consoler.log(ingredients[i])
+
+}
+
+for (i = ingredients.length - 1; i >= 0; i--) {
+  console.log(ingredients[i]);
+  }
